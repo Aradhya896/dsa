3edb96b0d6d5c999514d7f6dd4998eb71f020ebc
Aradhya896 <aradhyat896@gmail.com>
2026-09-25 09:22:43 +0530

    Time: 310 ms (5.01%), Space: 275.9 MB (5.13%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,14 +1,31 @@
 class Solution {
-    public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs,Comparator.comparingInt(o->o[1]));
-        int c=1;
-             int idx=pairs[0][1];
-        for(int i=1;i<pairs.length;i++){
-           if(pairs[i][0]>idx){
-            c++;
-            idx=pairs[i][1];
-           }
+    int dp[][]=new int[1001][1001];
+    public int findLongestChain(int[][] nums) {
+      
+Arrays.sort(nums, (a, b) -> a[0] - b[0]);
+    
+        for(int i[]:dp){
+        Arrays.fill(i,-99);
+           }   return  func(nums,0,-1);
+    }
+    public int func(int nums[][], int idx, int prevIdx){
+
+        if(idx==nums.length){
+            return 0;
         }
-        return c;
+        if(dp[idx][prevIdx+1]!=-99){
+           return  dp[idx][prevIdx+1];
+        }
+        
+        int skip=func(nums,idx+1,prevIdx);
+        int take=0;
+        if(prevIdx==-1 || nums[idx][0]>nums[prevIdx][1]){
+            take=1+func(nums,idx+1,idx);
+
+        }
+        
+       
+        dp[idx][prevIdx+1]=Math.max(skip,take);
+        return dp[idx][prevIdx+1];
     }
 }
